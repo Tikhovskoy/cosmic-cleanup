62d4924fec63742cbc24a296c8d49bd8f56ef270
Viktor Tikhovskoi <limbtsk@yandex.ru>
2025-07-21 17:14:31 +0300

    заменил asyncio на генераторы, реализовал run_event_loop

--- rocket_game.py
+++ rocket_game.py
@@ -1,6 +1,6 @@
-import asyncio
 import curses
 import random
+import time
 
 from curses_helpers import draw_frame, get_frame_size, read_controls
 
@@ -8,25 +8,25 @@ TIC_TIMEOUT = 0.1
 STAR_SYMBOLS = "+*.:"
 
 
-async def sleep_ticks(ticks=1):
+def sleep_ticks(ticks=1):
     for _ in range(ticks):
-        await asyncio.sleep(TIC_TIMEOUT)
+        yield
 
 
-async def blink(canvas, row, col, symbol="*", offset_ticks=1):
+def blink(canvas, row, col, symbol="*", offset_ticks=1):
     while True:
         canvas.addstr(row, col, symbol, curses.A_DIM)
         canvas.refresh()
-        await sleep_ticks(offset_ticks)
+        yield from sleep_ticks(offset_ticks)
         canvas.addstr(row, col, symbol)
         canvas.refresh()
-        await sleep_ticks(3)
+        yield from sleep_ticks(3)
         canvas.addstr(row, col, symbol, curses.A_BOLD)
         canvas.refresh()
-        await sleep_ticks(5)
+        yield from sleep_ticks(5)
         canvas.addstr(row, col, symbol)
         canvas.refresh()
-        await sleep_ticks(3)
+        yield from sleep_ticks(3)
 
 
 def split_frames(f1, f2):
@@ -39,12 +39,11 @@ def split_frames(f1, f2):
     return static, flame1, flame2, offset
 
 
-async def animate_spaceship(canvas, start_row, start_col, f1, f2):
+def animate_spaceship(canvas, start_row, start_col, f1, f2):
     max_rows, max_cols = canvas.getmaxyx()
     static, flame1, flame2, flame_offset = split_frames(f1, f2)
     row, col = start_row, start_col
     current_flame = flame1
-
     height, width = get_frame_size(f1)
 
     while True:
@@ -60,10 +59,21 @@ async def animate_spaceship(canvas, start_row, start_col, f1, f2):
         draw_frame(canvas, row + flame_offset, col, current_flame)
 
         canvas.refresh()
-        await sleep_ticks(2)
+        yield from sleep_ticks(2)
 
 
-async def draw(canvas):
+def run_event_loop(coroutines, canvas):
+    while True:
+        for coro in coroutines.copy():
+            try:
+                next(coro)
+            except StopIteration:
+                coroutines.remove(coro)
+        canvas.refresh()
+        time.sleep(TIC_TIMEOUT)
+
+
+def draw(canvas):
     curses.curs_set(False)
     canvas.nodelay(True)
     h, w = canvas.getmaxyx()
@@ -73,7 +83,7 @@ async def draw(canvas):
     with open("frames/rocket_frame_2.txt") as f:
         f2 = f.read()
 
-    tasks = [
+    coroutines = [
         blink(
             canvas,
             random.randint(1, h - 2),
@@ -85,13 +95,13 @@ async def draw(canvas):
     ]
 
     start_r, start_c = h // 2, w // 2
-    tasks.append(animate_spaceship(canvas, start_r, start_c, f1, f2))
+    coroutines.append(animate_spaceship(canvas, start_r, start_c, f1, f2))
 
-    await asyncio.gather(*(asyncio.create_task(t) for t in tasks))
+    run_event_loop(coroutines, canvas)
 
 
 def main():
-    curses.wrapper(lambda stdscr: asyncio.run(draw(stdscr)))
+    curses.wrapper(draw)
 
 
 if __name__ == "__main__":
